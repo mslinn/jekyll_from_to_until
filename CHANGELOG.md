7e35c07790df41fa7781b51f835d4f0f56d3cd80
Mike Slinn <mslinn@micronauticsresearch.com>
2022-04-05 17:54:16 -0400

    -

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,5 @@
 ## 1.0.1 / 2022-04-05
-  * Updated to `jekyll_plugin_logger` v2.0.2
+  * Updated to `jekyll_plugin_logger` v2.1.0
 
 ## 1.0.0 / 2022-03-14
   * Made into a Ruby gem and published on RubyGems.org as [jekyll_from_to_until](https://rubygems.org/gems/jekyll_from_to_until).
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,10 @@ require_relative '../lib/jekyll_from_to_until'
 Jekyll.logger.log_level = :info
 
 RSpec.configure do |config|
-  config.run_all_when_everything_filtered = true
   config.filter_run :focus
   config.order = 'random'
+  config.run_all_when_everything_filtered = true
+
+  # See https://relishapp.com/rspec/rspec-core/docs/command-line/only-failures
+  config.example_status_persistence_file_path = "spec/status_persistence.txt"
 end
